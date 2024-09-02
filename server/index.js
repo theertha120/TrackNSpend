ee7da784101865e26bfb35f9da8a6187d7c7ff0a
theertha120 <141700972+theertha120@users.noreply.github.com>
2024-09-02 19:25:27 +0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,38 +1,14 @@
 const express = require('express');
-const mysql = require('mysql2');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const authRoutes = require('./routes/authroutes');
-const connRoutes = require('./routes/connectionRoutes');
-const messageRoutes = require('./routes/messageRoutes');
-const pendingRequestsRoutes = require('./routes/pendingRequests'); 
+const expenditureRoutes = require('./routes/expenditures');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
-
 app.use(cors());
 app.use(bodyParser.json());
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'test',
-    database: 'artistically'
-});
+app.use('/api/expenditures', expenditureRoutes);
 
-connection.connect((err) => {
-    if (err) {
-        console.error('Error connecting to database:', err);
-        throw err;
-    }
-    console.log('Connected to MySQL database');
-});
-
-app.use('/auth', authRoutes);
-app.use('/messages', messageRoutes);
-app.use('/connections', connRoutes);
-app.use('/pending', pendingRequestsRoutes); // Use /pending instead of /pending-requests
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.listen(5000, () => {
+  console.log('Server running on port 5000');
 });
